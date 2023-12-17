180416948016dd27bf6b0cded4427398bbe05efe
seungwoo Kim <kyh196201@gmail.com>
2023-12-18 00:20:27 +0900

    style: 레이아웃, 헤더 스타일 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,18 @@
 import pretendard from './styles/fonts/pretendard'
 import '@/app/styles/globals.css'
 import Nav from '@/components/nav'
+import Link from 'next/link'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
-      <body className={pretendard.className}>
-        <header className={``}>
+      <body className={`${pretendard.className} pt-8 pb-32 px-4 mx-auto max-w-3xl min-w-[320px] min-h-screen`}>
+        <header className={`flex items-center justify-between mb-16`}>
+          {/* 로고 */}
+          <h1>
+            <Link href={`/`}>로고</Link>
+          </h1>
+
           <Nav />
         </header>
 
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,17 +1,37 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
 import navLinks from '../constants/navLinks'
 import Link from 'next/link'
 
-const Nav = () => {
+const NavItem = ({ title, link }: { title: string; link: string }) => {
+  const pathname = usePathname()
   return (
-    <nav>
+    <Link
+      href={link}
+      key={title}
+      className={`mx-1 transition-colors font-medium ${
+        pathname.startsWith(link) ? 'text-gray-900' : 'text-gray-900/60'
+      } hover:text-gray-900/80`}
+    >
+      <span className={`px-1 py-2`}>{title}</span>
+    </Link>
+  )
+}
+
+const Nav = () => {
+  const isDark = false
+
+  return (
+    <nav className={`flex items-center`}>
       {navLinks.map((nav) => (
-        <Link
-          href={nav.link}
+        <NavItem
+          {...nav}
           key={nav.title}
-        >
-          <span className={`mr-5`}>{nav.title}</span>
-        </Link>
+        />
       ))}
+
+      {isDark && <button type="button">다크모드</button>}
     </nav>
   )
 }
