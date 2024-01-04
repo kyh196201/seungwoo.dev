269b4b57cb4992c43eab416fe5745529064af5c9
seungwoo Kim <kyh196201@gmail.com>
2024-01-05 00:25:28 +0900

    refactor: posts 페이지 PostCardList 컴포넌트 사용

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,8 +1,8 @@
 import { Metadata } from 'next'
 import { DEFAULT_METADATA } from '@/utils/metadata'
 import postsService from '@/api/posts'
-import PostCard from '@/components/post-card'
 import PageLayout from '@/components/page-layout'
+import PostCardList from '@/components/post-card-list'
 
 export const metadata: Metadata = {
   ...DEFAULT_METADATA,
@@ -18,16 +18,7 @@ export default function Posts() {
       description="공유하고 싶고, 기록하고 싶은 내용을 위한 공간입니다."
     >
       <div className={`mt-12`}>
-        <ul className={`grid sm:grid-cols-2 gap-10 sm:gap-8`}>
-          {posts.map((post) => (
-            <li
-              key={post._id}
-              className={`animate-fadeInUp`}
-            >
-              <PostCard post={post} />
-            </li>
-          ))}
-        </ul>
+        <PostCardList posts={posts} />
       </div>
     </PageLayout>
   )
