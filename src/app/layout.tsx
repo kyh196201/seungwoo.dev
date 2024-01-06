e852c5180519d4c1a224d92bf90e0db9a05f7989
kyh196201 <kyh196201@google.com>
2024-01-06 23:42:48 +0900

    refactor: Layout header 영역 Header 컴포넌트로 분리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,7 @@
 import pretendard from './styles/fonts/pretendard'
 import '@/app/styles/globals.css'
 import Footer from '@/components/footer'
-import Nav from '@/components/nav'
-import Link from 'next/link'
+import Header from '@/components/header'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -10,19 +9,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body
         className={`${pretendard.className} flex flex-col py-8 px-4 mx-auto max-w-3xl min-w-[320px] min-h-screen xs:pt-12 xs:px-6`}
       >
-        <header className={`flex items-center justify-between mb-16`}>
-          {/* 로고 */}
-          <h1>
-            <Link
-              href={`/`}
-              className={`flex items-center gap-1 text-2xl font-bold`}
-            >
-              <span>sam.dev</span>
-            </Link>
-          </h1>
-
-          <Nav />
-        </header>
+        <Header />
 
         <main className={`flex-1`}>{children}</main>
 
---
+++ src/components/header.tsx
@@ -0,0 +1,24 @@
+import Nav from '@/components/nav'
+import Link from 'next/link'
+
+type Props = {}
+
+const Header = ({}: Props) => {
+  return (
+    <header className={`flex items-center justify-between mb-16`}>
+      {/* 로고 */}
+      <h1>
+        <Link
+          href={`/`}
+          className={`flex items-center gap-1 text-2xl font-bold`}
+        >
+          <span>sam.dev</span>
+        </Link>
+      </h1>
+
+      <Nav />
+    </header>
+  )
+}
+
+export default Header
