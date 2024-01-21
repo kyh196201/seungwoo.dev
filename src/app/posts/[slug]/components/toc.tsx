0d5dc2569b67948673fba2780c980ea4e4a17e51
seungwoo Kim <kyh196201@gmail.com>
2024-01-22 01:00:18 +0900

    feat: Next.js 라우터를 활용한 TOC 내비게이션 개선
    - TOC 클릭 시 url hash를 업데이트하도록 수정했습니다.

--- src/app/posts/[slug]/components/toc.tsx
+++ src/app/posts/[slug]/components/toc.tsx
@@ -3,6 +3,7 @@
 import { PostHeading as Heading } from 'contentlayer.config'
 import './toc.css'
 import { cn } from '@/utils'
+import { useRouter } from 'next/navigation'
 
 type Slug = Heading['slug']
 
@@ -12,16 +13,16 @@ type Props = {
 }
 
 const Toc = ({ headings, activeId }: Props) => {
+  const router = useRouter()
+
   const handleClick = (e: React.MouseEvent, slug: Slug) => {
     e.preventDefault()
+
+    router.push(`#${slug}`, { scroll: true })
 
     const hElement = document.getElementById(slug)
     if (hElement) {
       hElement.scrollIntoView({ block: 'start' })
-      window.scrollTo({
-        // 5 만큼을 더 이동
-        top: window.scrollY + 5,
-      })
     }
   }
 
