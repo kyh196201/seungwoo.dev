02d7e448c10b4f375b30bbf2d3843030916ec979
seungwoo Kim <kyh196201@gmail.com>
2024-01-23 00:29:28 +0900

    fix: TOC 클릭 이벤트 제거
    - a 태그의 기본 동작을 이용
    - 불필요한 router.push, scrollIntoView 로직 제거

--- src/app/posts/[slug]/components/toc.tsx
+++ src/app/posts/[slug]/components/toc.tsx
@@ -3,7 +3,6 @@
 import { PostHeading as Heading } from 'contentlayer.config'
 import './toc.css'
 import { cn } from '@/utils'
-import { useRouter } from 'next/navigation'
 
 type Slug = Heading['slug']
 
@@ -13,19 +12,6 @@ type Props = {
 }
 
 const Toc = ({ headings, activeId }: Props) => {
-  const router = useRouter()
-
-  const handleClick = (e: React.MouseEvent, slug: Slug) => {
-    e.preventDefault()
-
-    router.push(`#${slug}`, { scroll: true })
-
-    const hElement = document.getElementById(slug)
-    if (hElement) {
-      hElement.scrollIntoView({ block: 'start' })
-    }
-  }
-
   return (
     <div className="post-toc">
       <div className="post-toc-inner">
@@ -33,13 +19,12 @@ const Toc = ({ headings, activeId }: Props) => {
         <div>
           {headings.map(({ level, slug, text }) => (
             <div
-              key={`#${slug}`}
+              key={slug}
               className={cn(`post-toc-item`, activeId === slug ? 'active' : '')}
             >
               <a
                 data-level={level.toString()}
                 href={`#${slug}`}
-                onClick={(e) => handleClick(e, slug)}
               >
                 {text}
               </a>
