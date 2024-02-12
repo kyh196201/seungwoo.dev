e628fccd6168813a36f472d14686d2635d5802ac
seungwoo Kim <kyh196201@gmail.com>
2024-02-13 00:37:44 +0900

    fix: post 사이트맵 경로에 / 하나 더 들어가는 오류 수정

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,7 +9,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
   const posts: MetadataRoute.Sitemap =
     allPosts?.map((post) => ({
-      url: `${siteUrl}/${post.path}`,
+      url: `${siteUrl}${post.path}`,
       lastModified: new Date(post.date).toISOString(),
     })) ?? []
 
