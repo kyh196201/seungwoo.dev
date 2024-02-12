e85c7a4cf80e70352227a69e8a3ab1726ee7bf86
seungwoo Kim <kyh196201@gmail.com>
2024-02-12 18:53:08 +0900

    feat: notes와 하위 페이지는 사이트맵에 추가되지 않도록 수정

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,21 +4,22 @@ import { MetadataRoute } from 'next'
 import { CONFIG } from 'site.config'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  const posts = postsService.getAllPostByDateDesc()
+  const allPosts = postsService.getAllPostByDateDesc('post')
   const { siteUrl } = CONFIG
 
-  const blogPosts: MetadataRoute.Sitemap =
-    posts?.map((post) => ({
-      url: `${siteUrl}/posts/${post.slug}`,
+  const posts: MetadataRoute.Sitemap =
+    allPosts?.map((post) => ({
+      url: `${siteUrl}/${post.path}`,
       lastModified: new Date(post.date).toISOString(),
     })) ?? []
 
-  const navRoutes = navLinks.map((nav) => nav.link)
+  // notes 페이지는 사이트맵에 포함되지 않음
+  const navRoutes = navLinks.filter((nav) => nav.link !== '/notes').map((nav) => nav.link)
   const routes: MetadataRoute.Sitemap = ['', ...navRoutes].map((route) => ({
     url: `${siteUrl}${route}`,
     lastModified: new Date().toISOString(),
     priority: 0.7,
   }))
 
-  return [...blogPosts, ...routes]
+  return [...posts, ...routes]
 }
