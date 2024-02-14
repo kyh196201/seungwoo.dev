ecd42d1226b830a0b690a071b4c5aebb042b5eda
seungwoo Kim <kyh196201@gmail.com>
2024-02-15 00:29:52 +0900

    feat: nav menu 링크 클릭 시 메뉴 닫기

--- src/components/nav-menu.tsx
+++ src/components/nav-menu.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import { Menu } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet'
-import navLinks, { type NavLink } from '@/constants/navLinks'
+import navLinks from '@/constants/navLinks'
 
 const NavMenuTrigger = React.forwardRef<HTMLButtonElement>((props, ref) => {
   return (
@@ -21,12 +21,13 @@ const NavMenuTrigger = React.forwardRef<HTMLButtonElement>((props, ref) => {
 })
 NavMenuTrigger.displayName = 'NavMenuTrigger'
 
-const NavMenuItem = ({ title, link }: NavLink) => {
+const NavMenuItem = ({ title, link, onClick }: { title: string; link: string; onClick: () => void }) => {
   return (
     <li className="mb-4 last-of-type:mb-0">
       <Link
         href={link}
         className={`block px-1 py-2 transition-colors font-semibold text-link hover:text-link/80 focus:text-link/80`}
+        onClick={() => onClick()}
       >
         <span>{title}</span>
       </Link>
@@ -35,8 +36,14 @@ const NavMenuItem = ({ title, link }: NavLink) => {
 }
 
 const NavMenu = () => {
+  // https://github.com/shadcn-ui/ui/issues/88#issuecomment-1577482090
+  const [open, setOpen] = useState(false)
+
   return (
-    <Sheet>
+    <Sheet
+      open={open}
+      onOpenChange={setOpen}
+    >
       <SheetTrigger asChild>
         <NavMenuTrigger />
       </SheetTrigger>
@@ -46,6 +53,7 @@ const NavMenu = () => {
           {navLinks.map((nav) => (
             <NavMenuItem
               {...nav}
+              onClick={() => setOpen(false)}
               key={nav.title}
             />
           ))}
